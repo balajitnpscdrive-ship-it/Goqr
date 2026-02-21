764269da57ba80b026bcab006e12a7949806afae
balajitnpscdrive-ship-it <balajitnpscdrive@gmail.com>
2026-02-21 21:56:40 +0530

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker installed");
+self.addEventListener("install", event => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", function(event) {});
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {});
